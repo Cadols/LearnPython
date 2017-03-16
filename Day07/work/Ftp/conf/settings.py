701d9d28a4419f440edc1bb42cf58da83240553d
Cadols <wangwei150@gmail.com>
2017-03-16 09:34:53 +0800

    Day07 work settings

--- Day07/work/Ftp/conf/settings.py
+++ Day07/work/Ftp/conf/settings.py
@@ -4,6 +4,7 @@
 import os
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+DB_PATH = os.path.join(BASE_DIR, 'db')
 ACCOUNT_DB_PATH = os.path.join(BASE_DIR, 'db', 'account.db')
 USER_HOME_PATH = os.path.join(BASE_DIR, 'home')
 HOST = 'localhost'
