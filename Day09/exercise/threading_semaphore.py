aaee55029e1b2660d09af12c66f208db6f4fd387
Cadols <wangwei150@gmail.com>
2017-03-31 22:49:51 +0800

    threading semaphore

---
+++ Day09/exercise/threading_semaphore.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import time
+import threading
+
+
+def run(n):
+    semaphore.acquire()
+    time.sleep(1)
+    print("run the thread: %s\n" % n)
+    semaphore.release()
+
+if __name__ == '__main__':
+    semaphore = threading.BoundedSemaphore(5)  # 最多允许5个线程同时运行
+    for i in range(20):
+        t = threading.Thread(target=run, args=(i,))
+        t.start()
+
+while threading.active_count() != 1:
+    pass
+else:
+    print("------ All threads done ------")
